0eb03afb3dca09710dde2d157c6163e465cd12c7
khadizaliza <khadizaakter19836@gmail.com>
2022-07-01 22:10:31 +0600

    add-data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 
 
 import logo from './image/bill.webp';
+import { nanoid } from 'nanoid';
 import './App.css';
+import data from './bill.data.json';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [contacts, setContacts] = useState(data);
+  const [addFormData, setAddFormData] = useState({
+    
+    fullName: '',
+    email: '',
+    phone: '',
+    paidAmount: ''
+  });
+  const [editFormData, setEditFormData] = useState({
+    fullName: '',
+    email: "",
+    phone: "",
+    paidAmount: "",
+  });
+  const [editContactId, setEditContactId] = useState(null);
+  const handleAddFormChange = (event) =>{
+    event.preventDefult();
+    const fieldName = event.target.getattribute('name');
+    const fieldValue = event.target.value;
+    const newFormData = {...addFormData};
+    newFormData[fieldName] = fieldValue;
+    setAddFormData(newFormData);
+  };
+  const handleAddFormSubmit = (event) => {
+    event.preventDefult();
+    const newContact = {
+      id: nanoid(),
+      fullName: addFormData.fullName,
+      email: addFormData.email,
+      phone: addFormData.phone,
+      paidAmount: addFormData.paidAmount
+    };
+    const newContacts = [...contacts, newContact];
+    setContacts(newContacts);
+  }
   return (
     <div>
       <div  className="header">
@@ -20,7 +58,7 @@ function App() {
       <table>
         <thead>
           <tr>
-            <th>Billing ID</th>
+            
             <th>Full Name</th>
             <th>Email</th>
             <th>Phone</th>
@@ -28,15 +66,28 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Khadiza Akter</td>
-            <td>khadiza@gmail.com</td>
-            <td>333-444-3389</td>
-            <td>$ 344889</td>
+          {contacts.map((contact) => (
+            <tr>
+            
+            <td>{contact.fullName}</td>
+            <td>{contact.email}</td>
+            <td>{contact.phone}</td>
+            <td>{contact.paidAmount}</td>
           </tr>
+          ))}
+          
         </tbody>
       </table>
+      <h4>Add Contact</h4>
+      <form onSubmit={handleAddFormSubmit}>
+        
+        <input type="text" name='fullName' required="required" placeholder='Enter a name' onChange={handleAddFormChange}/>
+        <input type="text" name='email' required="required" placeholder='Enter a email' onChange={handleAddFormChange} />
+        <input type="text" name='phone' required="required" placeholder='Enter a phone' onChange={handleAddFormChange} />
+        <input type="text" name='paidAmount' required="required" placeholder='Enter a paidAmount' onChange={handleAddFormChange} />
+        <button type='submit'>Add</button>
+      </form>
+
       </div>
     </div>
   );
