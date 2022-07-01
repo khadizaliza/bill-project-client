51a87dcea8cbce78a38c92503931a6c8b4f41fad
khadizaliza <khadizaakter19836@gmail.com>
2022-07-01 18:24:28 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
+
+import logo from './image/bill.webp';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div  className="header">
+      <img src={logo} alt="" />
+      <p>Paid Total: 0</p>
+      </div>
+      
+      <div className='app-container'>
+      <div className='search-bar'>
+       <h6>Billings </h6>
+       <input type="search" placeholder='Search' />
+       <button className='btn btn-info'>Add New Bill</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Billing ID</th>
+            <th>Full Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Paid Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Khadiza Akter</td>
+            <td>khadiza@gmail.com</td>
+            <td>333-444-3389</td>
+            <td>$ 344889</td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
     </div>
   );
 }
